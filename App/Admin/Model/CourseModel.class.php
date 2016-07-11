08431c5ede9d1a112b3a808adab9dc65de7f5825
lff19920123 <18734832258@163.com>
2016-07-11 20:06:08 +0800

    admin

---
+++ App/Admin/Model/CourseModel.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace Admin\Model;
+use Think\Model\RelationModel;
+class CourseModel extends RelationModel
+{
+	protected $tableName = 'course';
+	protected $_link = array(
+		'ordera'=>array(
+			'mapping_type'=>self::HAS_MANY,
+			'foreign_key'=>'course_id'
+		),
+		'class'=>array(
+			'mapping_type'=>self::HAS_MANY,
+			'foreign_key'=>'course_id'
+		),
+		'bigpho'=>array(
+			'mapping_type'=>self::HAS_MANY,
+			'foreign_key'=>'course_id'
+		)
+	);
+}
+
+?>
